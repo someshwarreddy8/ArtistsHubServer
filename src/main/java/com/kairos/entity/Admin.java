7d45a577e9e5546f5b8e95df0e3494f734c31454
someshwarreddy8 <somudasari8@gmail.com>
2025-05-18 14:59:08 +0530

    updated primary key

--- src/main/java/com/kairos/entity/Admin.java
+++ src/main/java/com/kairos/entity/Admin.java
@@ -1,6 +1,9 @@
 package com.kairos.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,8 +12,10 @@ import lombok.ToString;
 @Getter
 @ToString
 @Entity
-public class Admin extends MetaDataEntity {
-
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String userName;
     private String password;
     private String confirmPassword;
